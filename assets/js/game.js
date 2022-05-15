ab31028ee8f834eb433ccb64add6129d19c53be9
yaakovbockian <yaakovbockian@gmail.com>
2022-05-15 15:59:50 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,13 +7,25 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Stephen Penis";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index ");
+// }
 // fight function
-var fight = function() {
-  // Alert players that they are starting the round
+var fight = function(enemyName) {
+  
+    // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
@@ -58,9 +70,11 @@ var fight = function() {
       playerMoney = playerMoney - 2;
     }
     // if no (false), ask question again by running fight() again
+    
     else {
-      fight();
+        fight()
     }
+    
     // if player did not chose 1 or 2 in prompt
   } else {
     window.alert("You need to pick a valid option. Try again!");
@@ -68,4 +82,6 @@ var fight = function() {
 }; // end of fight function
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
